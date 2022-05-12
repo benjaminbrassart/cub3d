65a3389821eb06f3ea9ff0d67357ada82c8c6a46
maxy60 <cest-mon-yop@hotmail.fr>
2022-05-12 17:14:57 +0200

    set work

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: msainton <msainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 11:18:33 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/12 12:35:30 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/05/12 15:33:55 by msainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int	main(int argc, char const *argv[])
 {
 	if (args_check(argc, argv) == RES_FAILURE)
 		return (EXIT_FAILURE);
+	
 	return (EXIT_SUCCESS);
 }
--- src/parse/parse.c
+++ src/parse/parse.c
@@ -1,38 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   args_check.c                                       :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msainton <msainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/12 11:30:50 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/12 15:01:56 by msainton         ###   ########.fr       */
+/*   Created: 2022/05/12 15:15:19 by msainton          #+#    #+#             */
+/*   Updated: 2022/05/12 16:25:58 by msainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "args.h"
-#include "cuberr.h"
 #include "def.h"
-
-#include "ft.h"
 
 #include <unistd.h>
 
-int	args_check(int argc, char const *argv[])
+int	check_map(int fd)
 {
-	char	*ext;
-
-	if (argc != 2)
+	char	*line;
+	int		res;
+	
+	while (res > 0)
 	{
-		print_error("usage", PROGNAME " <map.cub>");
-		return (RES_FAILURE);
+		if (res = get_next_line(fd, &line) == -1)
+			return (RES_FAILURE);
+		
 	}
-	ext = ft_strrstr(argv[1], FILEEXT);
-	if (ext == NULL || ft_strcmp(ext, FILEEXT) != 0)
-	{
-		print_error(argv[1], "File extension must be " FILEEXT);
-		return (RES_FAILURE);
-	}
-	return (RES_SUCCESS);
+	
 }
-
