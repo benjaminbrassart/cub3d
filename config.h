38eb9aa7cb5b53cd5d313a261be7e6b10f380e17
Benjamin BRASSART <bbrassar@student.42.fr>
2022-05-12 17:48:34 +0200

    Added simple input key handler

--- config.h
+++ config.h
@@ -6,15 +6,18 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 14:16:17 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/12 14:17:01 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/05/12 17:46:53 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIG_H
 # define CONFIG_H
 
-# define WIN_WIDTH	500
-# define WIN_HEIGHT	400
-# define WIN_TITLE	"CUB3D"
+# define WIN_WIDTH		500
+# define WIN_HEIGHT		400
+# define WIN_TITLE		"CUB3D"
+
+# define MOVEMENT_SPEED	1.0f
+# define CAMERA_SPEED	1.0f
 
 #endif
--- src/mlx_handler/input_key_handle.c
+++ src/mlx_handler/input_key_handle.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 15:06:02 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/12 15:11:33 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/05/12 17:47:06 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,53 @@
 
 #include <X11/keysym.h>
 
+static void	_move_player_x(t_cub *cub, int factor);
+static void	_move_player_y(t_cub *cub, int factor);
+static void	_move_player_yaw(t_cub *cub, int factor);
+
 int	input_key_handle(int key, t_cub *cub)
 {
+	int	factor_x;
+	int	factor_y;
+	int	factor_yaw;
+
+	factor_x = 0;
+	factor_y = 0;
+	factor_yaw = 0;
 	if (key == XK_Escape)
 		return (destroy_handle(cub));
+	if (key == XK_W)
+		--factor_y;
+	if (key == XK_S)
+		++factor_y;
+	if (key == XK_D)
+		++factor_x;
+	if (key == XK_A)
+		--factor_x;
+	if (key == XK_Left)
+		--factor_yaw;
+	if (key == XK_Right)
+		++factor_yaw;
+	_move_player_x(cub, factor_x);
+	_move_player_y(cub, factor_y);
+	_move_player_yaw(cub, factor_yaw);
 	return (0);
+}
+
+static void	_move_player_x(t_cub *cub, int factor)
+{
+	(void)cub;
+	(void)factor;
+}
+
+static void	_move_player_y(t_cub *cub, int factor)
+{
+	(void)cub;
+	(void)factor;
+}
+
+static void	_move_player_yaw(t_cub *cub, int factor)
+{
+	(void)cub;
+	(void)factor;
 }
