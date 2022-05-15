25bf56617fe3f97bc4490092e58ade0ef0788b4d
Benjamin BRASSART <bbrassar@student.42.fr>
2022-05-15 13:02:46 +0200

    Decreased default player speed

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 14:16:17 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/12 18:35:36 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/05/15 12:55:59 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # define WIN_HEIGHT		400
 # define WIN_TITLE		"CUB3D"
 
-# define MOVEMENT_SPEED	5.0f
+# define MOVEMENT_SPEED	0.02f
 // RADIANS
 # define CAMERA_SPEED	1.0f
 
