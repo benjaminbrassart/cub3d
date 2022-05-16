b827748fcf43546bb6a27af8f906c959d18c6a03
maxy60 <cest-mon-yop@hotmail.fr>
2022-05-16 12:41:51 +0200

    add table pointeur

--- include/parse.h
+++ include/parse.h
@@ -3,21 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   parse.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
+/*   By: msainton <msainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 17:44:55 by maxime            #+#    #+#             */
-/*   Updated: 2022/05/14 18:05:13 by maxime           ###   ########.fr       */
+/*   Updated: 2022/05/16 12:13:22 by msainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
+typedef struct s_param
+{
+	char	*direction;
+	char	*img;
+}	t_param;
+
 typedef struct s_map
 {
-	char	*my_map;
-	int		fd;
+	char	*elem;
+	char	*img;
+	int		(*func)(char *line, t_param *param);
 }	t_map;
+
+
+static t_map const	g_map[] = {
+{"NO", path_to_the_north_texture, texture},
+{"SO", path_to_the_south_texture, texture},
+{"WE", path_to_the_west_texture, texture},
+{"EA", path_to_the_east_texture, texture},
+{"F", RGB, color},
+{"C", RGB, color},
+};
 
 int		check_map(int fd);
 
--- src/parse/parse.c
+++ src/parse/parse.c
@@ -3,21 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
+/*   By: msainton <msainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 15:15:19 by msainton          #+#    #+#             */
-/*   Updated: 2022/05/15 01:18:48 by maxime           ###   ########.fr       */
+/*   Updated: 2022/05/16 12:27:28 by msainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "def.h"
+#include "ft.h"
+#include "parse.h"
 
 #include <unistd.h>
 
-
-void	check_line(char *line)
+int	custom_isspace(int c)
 {
-	
+	return (c == ' ' || c == '\t' || c == '\v' || c == '\f' || c == '\r');
+}
+
+int	texture(char *line, t_param *param)
+{
+	if (g_map[0])
 }
 
 int	check_map(int fd)
@@ -30,7 +36,5 @@ int	check_map(int fd)
 		res = get_next_line(fd, &line);
 		if (res <= 0)
 			break ;
-		check_line(line);
 	}
-	
 }
