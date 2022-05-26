d9ebf404e4f7127f34ffb5f0c386f86efc46828b
Benjamin BRASSART <bbrassar@student.42.fr>
2022-05-26 11:17:27 +0200

    Changed default window size

--- config.h
+++ config.h
@@ -6,15 +6,15 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 14:16:17 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/19 11:43:16 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/05/26 10:51:20 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIG_H
 # define CONFIG_H
 
-# define WIN_WIDTH			2000
-# define WIN_HEIGHT			1000
+# define WIN_WIDTH			800
+# define WIN_HEIGHT			600
 # define WIN_TITLE			"CUB3D"
 
 # define MM_TILE_SIZE		8
