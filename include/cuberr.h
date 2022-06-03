fae4c1e044d3eb08b3bd68d885b31dc2fc32710b
Benjamin BRASSART <bbrassar@student.42.fr>
2022-06-03 10:22:53 +0200

    Added macros for every error message

--- include/cuberr.h
+++ include/cuberr.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 11:36:48 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/12 11:44:06 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/06/03 10:17:45 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,26 @@
 # define CUBERR_H
 
 # include "def.h"
+
+# define ERROR_USAGE				"cub3d <map.cub>"
+# define ERROR_EXTENSION			"File extension must be "
+
+# define ERROR_INIT_DISPLAY			"failed to initialize display"
+# define ERROR_INIT_WINDOW			"failed to initialize window"
+# define ERROR_INIT_CANVAS			"failed to initialize canvas"
+
+# define ERROR_MAP_NO_SPACE			"not enough space"
+# define ERROR_MAP_SURROUND			"not surrounded by walls"
+# define ERROR_MAP_HOLE				"empty space next to floor"
+# define ERROR_MAP_SPAWN_DUPLICATED	"duplicated spawn"
+# define ERROR_MAP_NO_SPAWN			"no spawn found"
+
+# define ERROR_PARAM_DUPLICATED		"duplicated identifier"
+# define ERROR_PARAM_MISSING		"missing indentifier"
+
+# define ERROR_COLOR_TRAILING		"trailing character"
+# define ERROR_COLOR_BOUNDS			"value out of bounds"
+# define ERROR_COLOR_FORMAT			"invalid color format"
 
 /**
  * Print a message on the standard error, followed by a newline
