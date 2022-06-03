d3a5bea27eff8adf681b00434390e557599be2a8
Benjamin BRASSART <bbrassar@student.42.fr>
2022-06-03 14:33:27 +0200

    Improved collisions

--- src/player/player_move.c
+++ src/player/player_move.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   player_move.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msainton <msainton@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 13:27:37 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/06/02 15:25:50 by msainton         ###   ########.fr       */
+/*   Updated: 2022/06/03 14:31:51 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,17 @@
 #include "cub.h"
 
 #include <math.h>
-#include <stdbool.h>
-
-static int	_check_collision(t_cub const *cub, float move_x, float move_y)
-{
-	if (map_gettile(cub, cub->player.x + move_x, cub->player.y + move_y) == '1')
-		return (0);
-	return (1);
-}
 
 void	player_move(t_cub *cub, float angle)
 {
-	float const	move_x = MOVEMENT_SPEED * cos(cub->player.yaw + angle);
-	float const	move_y = MOVEMENT_SPEED * sin(cub->player.yaw + angle);
+	float const	mx = MOVEMENT_SPEED * cos(cub->player.yaw + angle);
+	float const	my = MOVEMENT_SPEED * sin(cub->player.yaw + angle);
+	t_ray		ray;
 
-	if (_check_collision(cub, move_x, move_y))
+	ray_init(&ray, &cub->player, cub->player.yaw + angle);
+	if (!ray_cast(cub, &ray, 0.1) || ray.distance > 0.1)
 	{
-		cub->player.x += move_x;
-		cub->player.y += move_y;
+		cub->player.x += mx;
+		cub->player.y += my;
 	}
 }
